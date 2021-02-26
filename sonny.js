7a44e878901f89b422005334f3a8b7fba091c965
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-02-26 20:56:25 -0300

    new version

--- sonny.js
+++ sonny.js
@@ -4,17 +4,20 @@ const brain = require('brain.js')
 const net = new brain.recurrent.LSTM()
 
 let array
-
+// teste de performance/tempo de aprendizado
+let initTime = Date.now();
+// configuração readline
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// verifica se o json de treinamento está preenchido
 fs.readFile('learned.json', (err, data) => {
   if (err) return console.log(err)
 
   if (data.toString() === '') {
-    console.log('A Network já foi treinada')
+    console.log('Sonny será treinado agora!')
     train();
   } else {
     net.fromJSON(JSON.parse(data.toString()))
@@ -22,6 +25,7 @@ fs.readFile('learned.json', (err, data) => {
   }
 })
 
+// realiza o treinamento
 const train = () => {
   console.log('treinando sonny...')
   fs.readFile('intents.json', (err, data) => {
@@ -29,7 +33,8 @@ const train = () => {
     let jsonFile = JSON.parse(array)
     let inOuts = []
     jsonFile.intents.forEach(element => {
-      if (element.tag == 'greeting') {
+      // tempo de treino 2 horas ou mais
+      if (element.tag == 'saudacoes' || element.tag == 'cardapio_opcoes' || element.tag == 'cardapio_escolhas') {
         for (let i = 0; i < element.input.length; i++) {
           inOuts.push({
             input: element.input[i],
@@ -38,15 +43,20 @@ const train = () => {
         }
       }
     });
-    console.log(inOuts)    
-    net.train(inOuts);
+    // console.log(inOuts)    
+    net.train(inOuts, {
+      log: true,
+    });
     fs.writeFileSync('learned.json', JSON.stringify(net.toJSON()), (err, result) => {
       if (err) return console.log(err)
     })
     console.log('Treinamento finalizado')
+    console.log("Sonny foi treinado com 20000 iterações em: ", Date.now() - initTime, ' milisegundos')
+    boot();
   })
 }
 
+// inicia o bot caso já tenha sido treinado
 const boot = () => {
   rl.question('Me: ', (q) => {
     console.log(`Sonny: ${net.run(q)}`)

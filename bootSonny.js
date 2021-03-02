71ae1d53ed6dbc10e144112db77c646c5b2335eb
Pedro Mattos <laranjeiraspedro@gmail.com>
2021-03-02 13:55:13 -0300

    primeira versão estável do boot

--- bootSonny.js
+++ bootSonny.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const readline = require('readline')
 const brain = require('brain.js');
-const {Sonny} = require('./testSonny')
+// const {Sonny} = require('./testSonny')
 const net = new brain.recurrent.LSTM()
 
 const rl = readline.createInterface({
@@ -10,12 +10,13 @@ const rl = readline.createInterface({
 });
 
 class BootSonny {
-  constructor (learnedIntentsDir) {
+  constructor (learnedIntentsDir, intentFileDir) {
     this.learnedIntentsDir = learnedIntentsDir == undefined ? './learnedIntents/' : learnedIntentsDir
+    this.intentFileDir = intentFileDir == undefined ? 'testeIntents.json' : intentFileDir
 
     /**
      * 
-     * @param {String} intentsDir Recebe o diretório de onde se encontram as intents aprendidas por Sonny
+     * @param {String} learnedIntentsDir Recebe o diretório de onde se encontram as intents aprendidas por Sonny
      * @returns {Array<String>} Retorna todos os arquivos dentro do diretório de intents aprendidas
      */
     BootSonny.FindLocalFiles = (learnedIntentsDir) => {
@@ -23,7 +24,6 @@ class BootSonny {
     }
 
     BootSonny.prototype.StartBoot = () => {
-      console.log('Booting')
       let filesInLearnedDir = BootSonny.FindLocalFiles(this.learnedIntentsDir)
       if (filesInLearnedDir.length > 0) {
         BootSonny.prototype.CheckContentFile(filesInLearnedDir)
@@ -33,13 +33,33 @@ class BootSonny {
     }
 
     BootSonny.prototype.CheckContentFile = (fileName) => {
-      console.log(fileName)
       for (let i = 0; i < fileName.length; i++) {
         let fileBuffer = fs.readFileSync(this.learnedIntentsDir + fileName[i])
         if (fileBuffer.toString() === '') {
-          throw new Error('Sonny ainda não aprendeu nenhuma intent, por favor execute o treinamento')
+          // tratar de outra maneira
+          // throw new Error('Sonny ainda não aprendeu nenhuma intent, por favor execute o treinamento')
         } else {
           // proximo passo, receber o input do usuário e comparar com os dados nas intents
+          rl.question('Me: ', (q) => {
+            let sonnyBooted = new BootSonny
+            let tagIntentDetected = BootSonny.GetContentFile(q)
+            fs.readFile(this.learnedIntentsDir + tagIntentDetected + '.json', (err, data) => {
+              if (err) return console.log(err)
+              net.fromJSON(JSON.parse(data.toString()))
+              console.log(`Sonny: ${net.run(q)}`)
+              sonnyBooted.boot()
+            })
+          })
+        }
+      }
+    }
+
+    BootSonny.GetContentFile = (question) => {
+      let fileBuffer = fs.readFileSync(this.intentFileDir)
+      let jsonFileContent = JSON.parse(fileBuffer.toString())
+      for (let i = 0; i < jsonFileContent.intents.length; i++) {
+        if (jsonFileContent.intents[i].input.indexOf(question) > -1) {
+          return jsonFileContent.intents[i].tag
         }
       }
     }
